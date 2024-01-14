7e2970ee5d41dd73c52e8aa7eaac24f721ca9a72
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-14 14:22:58 +0330

    Dadane etelaat dar js

---
+++ main.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+      id: 1,
+      name: "tahsin soufizade",
+      job: "FrontEnd Developer",
+      img: "./assets/1-user.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad asperiores autem beatae dolore, ducimus eveniet illum labore maiores odio, odit officia pariatur perspiciatis provident quidem repellat sequi tempora totam vitae voluptatibus! Amet cum dolor fugiat iusto tempore! Autem, fuga.",
+    },
+    {
+      id: 2,
+      name: "Mahsa Golabi",
+      job: "Barber",
+      img: "./assets/2-user.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad asperiores autem ore maiores odio, odit officia pariatur perspiciatis provident quidem repellat sequi tempora totam vitae voluptatibus! Amet cum dolor fugiat iusto tempore! Autem, fuga.",
+    },
+    {
+      id: 3,
+      name: "Ghasem Kargar",
+      job: "Baker",
+      img: "./assets/3-user.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipe maiores odio, odit officia pariatur perspiciatis provident quidem repellat sequi tempora totam vitae voluptatibus! Amet cum dolor fugiat iusto tempore! Autem, fuga.",
+    },
+    {
+      id: 4,
+      name: "Helma mahdavi",
+      job: "BackEnd Developer",
+      img: "./assets/4-user.jpg",
+      text: "Lorem ipsum dolor sit amet,maiores odio, odit officia pariatur perspiciatis provident quidem repellat sequi tempora totam vitae voluptatibus! Amet cum dolor fugiat iusto tempore! Autem, fuga.",
+    },
+  ];
